d7a1d9c5516950fcfe837ac5c87800dfbd5bf78b
Hugo Felipe Torrico Marquez <htorrico@truenorthcorporation.com>
2020-01-14 08:38:49 -0500

    Agregar archivos de proyecto.

---
+++ NotificacionExmple/Hubs/NotificationHub.cs
@@ -0,0 +1,24 @@
+﻿
+
+namespace NotificacionExmple.Hubs
+{
+    using Microsoft.AspNet.SignalR;
+    public class NotificationHub : Hub
+    {
+        /// <summary>
+        /// Acción de notificación de una tarea completa.
+        /// </summary>
+        /// <param name="taskId">Identificador de la tarea.</param>
+        /// <param name="taskResult">Resultado de la tarea.</param>
+        public void Notificate(int taskId, bool taskResult)
+        {
+
+            Clients.All.markDone(taskId, taskResult);
+        }
+
+        public void NotificateHello(string message)
+        {
+            Clients.All.hello(message);
+        }
+    }
+}
---
+++ NotificacionExmple/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(NotificacionExmple.Startup))]
+namespace NotificacionExmple
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
